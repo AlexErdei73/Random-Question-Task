c3c0bca423f63bb23ba01c07e2486c0e44731106
Alex Erdei <mralexerdei@yahoo.co.uk>
2023-05-19 13:07:56 +0100

    Add solution function to question.js

--- utilities/question.js
+++ utilities/question.js
@@ -4,12 +4,33 @@ export function question(input) {
   const d = b * x + a * y;
   const lines = [];
   lines.push(
-    `${a}kg of potatoes an ${b}kg of carrots have a total coast of ${c}p.`
+    `${a}kg of potatoes and ${b}kg of carrots have a total coast of ${c}p.`
   );
   lines.push(
-    `${b}kg of potatoes an ${a}kg of carrots have a total coast of ${d}p.`
+    `${b}kg of potatoes and ${a}kg of carrots have a total coast of ${d}p.`
   );
   lines.push("");
   lines.push("Work out the total coast of 1kg of potatoes and 1kg of carrots.");
+  return lines;
+}
+
+export function solution(input) {
+  const { a, b, x, y } = input;
+  const c = a * x + b * y;
+  const d = b * x + a * y;
+  const answer = Math.floor((c + d) / (a + b));
+  const lines = [];
+  lines.push(
+    "If x is the cost of 1kg of potatoes and y is the cost of 1kg of carrots"
+  );
+  lines.push(`${a}x + ${b}y = ${c}`);
+  lines.push(`${b}x + ${a}y = ${d}`);
+  lines.push("Add up the equations to get");
+  lines.push(`${a + b}x + ${a + b}y = ${c + d}`);
+  lines.push(`Divide both sides by ${a + b}`);
+  lines.push(`x + y = ${answer}`);
+  lines.push(
+    `1kg of potatoes and 1kg of carrots have a total cost of ${answer}p.`
+  );
   return lines;
 }
